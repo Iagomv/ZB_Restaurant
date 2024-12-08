abc7ea1cf4b6bb90af07e071e2383e4660ef8319
Iagomv <iagomartinezv@gmail.com>
2024-12-08 22:10:59 +0100

    API en funcionamiento

--- ZBAPI/Servidor.js
+++ ZBAPI/Servidor.js
@@ -1,25 +1,11 @@
-require('dotenv').config() // Para poder obtener las variables dentro de .env
-
 const express = require('express')
 const app = express()
-const path = require('path')
-mongoose.connect(process.env.DATABASE_URL)
-const db = mongoose.connection
+const router = require('./routes/index')
+const PORT = 6240
 
-// Servir archivos estáticos desde la carpeta `public`
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use('/api', router)
 
-// Ruta para cargar el frontend (HTML) en el navegador
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public', 'index.html'))
+app.listen(PORT, () => {
+	console.log(`Servidor escuchando en http://localhost:${PORT}`)
 })
-
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Conectado a la base de datos'))
-
-app.use(express.json()) // Middleware para aceptar json en todas las rutas
-
-const tareasRouter = require('./routes/tareas')
-app.use('/api/tareas', tareasRouter)
-
-app.listen(3000, () => console.log('Server started'))
---
+++ ZBAPI/model/Persona.js
@@ -0,0 +1,14 @@
+class Persona {
+	constructor(id_personal, nombre, apellidos, dni, edad, fecha_inicio, fecha_fin_contrato, horas_semanales, rol, imagen) {
+		this.id_personal = id_personal
+		this.nombre = nombre
+		this.apellidos = apellidos
+		this.dni = dni
+		this.edad = edad
+		this.fecha_inicio = fecha_inicio
+		this.fecha_fin_contrato = fecha_fin_contrato
+		this.horas_semanales = horas_semanales
+		this.rol = rol
+		this.imagen = imagen
+	}
+}
