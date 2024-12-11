0a3c80a8a25b07b4930836414ce9c08edf45657a
iagoK <iago.martinez.varela@colexio-karbo.com>
2024-12-11 19:14:06 +0100

    Pendiente obtener menu

---
+++ ZBSimulacion/API/ConsultasAPI.java
@@ -0,0 +1,8 @@
+package API;
+
+import java.net.URI;
+import java.net.URL;
+
+public class ConsultasAPI {
+    URI url = new URI("http://localhost:8080/Restaurante/consultas");
+}
---
+++ ZBSimulacion/API/JSONReader.java
@@ -0,0 +1,96 @@
+package API;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class JSONReader {
+    private ArrayList<Pokemon> pokeList;
+    private String previousUrl;
+    private String nextUrl;
+
+    public JSONReader(String url) {
+        try {
+            obtenerDatos(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // * leearchivo -> obtenerPokemons -> obtenemos Pokemons y URLS*/
+    private void obtenerDatos(String url) throws Exception {
+        pokeList = new ArrayList<>();
+        JSONObject json = leerArchivo(url);
+        if (json != null) {
+            // Extraer Pokémon y URLs
+            JSONArray jsonArray = (JSONArray) json.get("results");
+            obtenerPokemons(pokeList, jsonArray);
+            previousUrl = (String) json.get("previous");
+            nextUrl = (String) json.get("next");
+        }
+
+        else {
+            System.out.println("wtf no hay JSON?");
+        }
+    }
+
+    // Método Moncho URL -> JSONObject/
+    public static JSONObject leerArchivo(String url) throws Exception {
+        InputStream is = null;
+        BufferedReader br = null;
+        try {
+            String linea;
+            StringBuilder jsonText = new StringBuilder();
+
+            is = new URL(url).openStream();
+            br = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            while ((linea = br.readLine()) != null) {
+                jsonText.append(linea);
+            }
+
+            JSONParser parser = new JSONParser();
+            return (JSONObject) parser.parse(jsonText.toString());
+        } catch (Exception e) {
+            System.out.println("Hubo un error al obtener el JSON de la URL: " + e.getMessage());
+            return null;
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+            if (br != null) {
+                br.close();
+            }
+        }
+    }
+
+    // Loop -> Instanciamos nuevo pokemon y añadimos a lista
+    private static void obtenerPokemons(ArrayList<Pokemon> pokeList, JSONArray jsonArray) {
+        for (Object result : jsonArray) {
+            JSONObject pokeJson = (JSONObject) result;
+            Pokemon pokemon = new Pokemon(
+                    pokeJson.get("name").toString(),
+                    pokeJson.get("url").toString());
+            pokeList.add(pokemon);
+            // System.out.println(pokemon.getPokeNumero());
+        }
+    }
+
+    public ArrayList<Pokemon> getPokemonList() {
+        return pokeList;
+    }
+
+    public String getPreviousUrl() {
+        return previousUrl;
+    }
+
+    public String getNextUrl() {
+        return nextUrl;
+    }
+
+}
